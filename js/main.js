bc0573e23b89c3c28167025d6f7f9a8335a429d2
Pavel Tsyganov <tsyganov.p@gmail.com>
2021-06-07 21:30:04 +0300

    Заменил return в ошибках на throw new Error

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
 function getRandomIntInclusive(min, max) {
   if (min < 0 || max < 0) {
-    return 'Введите корректное число';
+    //return 'Введите корректное число';
+    throw new Error("Максимальное число меньше минимального");
   }
   if (max < min) {
-    return 'Максимальное число меньше минимального';
+    //return 'Максимальное число меньше минимального';
+    throw new Error("Максимальное число меньше минимального");
   }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomIntInclusive(10, 1);
+getRandomIntInclusive(1, 10);
 
 function getRandomFloat(min, max, numbersAfter) {
   if (min < 0 || max < 0) {
-    return 'Введите корректное число';
+    //return 'Введите корректное число';
+    throw new Error("Введите корректное число");
   }
   if (max < min) {
-    return 'Максимальное число меньше минимального';
+    //return 'Максимальное число меньше минимального';
+    throw new Error ("Максимальное число меньше минимального");
   }
   return (Math.random() * (max - min) + min).toFixed(numbersAfter);
 }
@@ -66,6 +70,7 @@ const createLocation =() => {
     lng: getRandomFloat(lng_minimum, lng_maximum, 4),
   };
 }
+
 console.log(createAuthor());
 console.log(createOffer());
 console.log(createLocation());
