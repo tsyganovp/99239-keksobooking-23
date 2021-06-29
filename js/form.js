8dc51740ea2ecb2770ea222c5efe3d9f18b0f9da
Pavel Tsyganov <tsyganov.p@gmail.com>
2021-06-29 18:52:51 +0300

    Сделал функции блокировки и разблокировки формы

---
+++ js/form.js
@@ -0,0 +1,35 @@
+//let mapBlock = document.querySelector('.map__canvas');
+let form = document.querySelector('.ad-form');
+let formFieldsets = form.querySelectorAll('fieldset');
+let filter = document.querySelector('.map__filters');
+
+const setDisable = function () {
+  form.classList.add('ad-form--disabled');
+
+  for(let i = 0; i <= formFieldsets.length-1; i++) {
+    formFieldsets[i].setAttribute('disabled', true);
+  }
+
+  filter.classList.add('map__filters--disabled');
+
+  for(let i = 0; i <= filter.length - 1; i++) {
+    filter[i].setAttribute('disabled', true);
+  }
+}
+
+const setEnable = function() {
+  form.classList.remove('ad-form--disabled');
+
+  for(let i = 0; i <= formFieldsets.length-1; i++) {
+    formFieldsets[i].setAttribute('disabled', false);
+  }
+
+  filter.classList.remove('map__filters--disabled');
+
+  for(let i = 0; i <= filter.length - 1; i++) {
+    filter[i].setAttribute('disabled', false);
+  }
+
+}
+
+export {setDisable, setEnable};
