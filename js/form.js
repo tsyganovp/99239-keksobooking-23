dece1b12dcfc366f125720e461b051eb25a7bc03
Pavel Tsyganov <tsyganov.p@gmail.com>
2021-06-29 19:29:08 +0300

    Сделал функции блокировки и разблокировки формы

--- js/form.js
+++ js/form.js
@@ -1,35 +1,35 @@
 //let mapBlock = document.querySelector('.map__canvas');
-let form = document.querySelector('.ad-form');
-let formFieldsets = form.querySelectorAll('fieldset');
-let filter = document.querySelector('.map__filters');
+const FORM = document.querySelector('.ad-form');
+const FORM_FIELDSETS = FORM.querySelectorAll('fieldset');
+const FILTER = document.querySelector('.map__filters');
 
 const setDisable = function () {
-  form.classList.add('ad-form--disabled');
+  FORM.classList.add('ad-form--disabled');
 
-  for(let i = 0; i <= formFieldsets.length-1; i++) {
-    formFieldsets[i].setAttribute('disabled', true);
+  for(let i = 0; i <= FORM_FIELDSETS.length-1; i++) {
+    FORM_FIELDSETS[i].setAttribute('disabled', true);
   }
 
-  filter.classList.add('map__filters--disabled');
+  FILTER.classList.add('map__filters--disabled');
 
-  for(let i = 0; i <= filter.length - 1; i++) {
-    filter[i].setAttribute('disabled', true);
+  for(let i = 0; i <= FILTER.length - 1; i++) {
+    FILTER[i].setAttribute('disabled', true);
   }
-}
+};
 
 const setEnable = function() {
-  form.classList.remove('ad-form--disabled');
+  FORM.classList.remove('ad-form--disabled');
 
-  for(let i = 0; i <= formFieldsets.length-1; i++) {
-    formFieldsets[i].setAttribute('disabled', false);
+  for(let i = 0; i <= FORM_FIELDSETS.length-1; i++) {
+    FORM_FIELDSETS[i].setAttribute('disabled', false);
   }
 
-  filter.classList.remove('map__filters--disabled');
+  FILTER.classList.remove('map__filters--disabled');
 
-  for(let i = 0; i <= filter.length - 1; i++) {
-    filter[i].setAttribute('disabled', false);
+  for(let i = 0; i <= FILTER.length - 1; i++) {
+    FILTER[i].setAttribute('disabled', false);
   }
+};
 
-}
 
 export {setDisable, setEnable};
